ad775bb567546a89ffb09535b45b3d36497c2277
Ashley Domineck <adouglas0721@gmail.com>
2025-08-15 14:28:54 -0400

    Part 3

--- scripts/script.js
+++ scripts/script.js
@@ -21,10 +21,21 @@ topMenuEl.classList.add('flex-around');
 
 //Part 3
 var menuLinks = [
-  { text: 'about', href: '/about' },
-  { text: 'catalog', href: '/catalog' },
-  { text: 'orders', href: '/orders' },
-  { text: 'account', href: '/account' },
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
 //Iterate over the entire menuLinks array and for each "link" object:
@@ -43,4 +54,30 @@ topMenuEl.appendChild(a);
 
 })
 
-////-
+// Part 1 Geeting started (completed)
+
+//Part 2: Adding Additional HTML and CSS (completed)
+
+//Part 3: Creating the Submenu--------------------------------------------
+
+// Select and cache the <nav id="sub-menu">
+const subMenuEl = document.getElementById('sub-menu');
+
+// Set height to 100%
+subMenuEl.style.height = '100%';
+
+// Set background color using CSS variable
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+// Add flex-around class
+subMenuEl.classList.add('flex-around');
+
+//Part 4: Adding Menu Interaction
+// Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+
+// Attach a delegated 'click' event listener to topMenuEl.
+    // The first line of code of the event listener function should call the event object's preventDefault() method.
+    
+    // The second line of code of the function should immediately return if the element clicked was not an <a> element.
+    
+    // Log the content of the <a> to verify the handler is working.
